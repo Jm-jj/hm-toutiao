b6c2d654fe577d77a26b6e47b05d864d4f9ef6fc
YJM <yzdyddx@163.com>
2020-02-10 14:48:58 +0800

    axios请求拦截

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,16 @@ import axios from 'axios'
 import auth from '@/utils/auth'
 
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
-axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
+// axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
 
-
-
+// 请求拦截器
+axios.interceptors.request.use((config) => {
+  const user = auth.getUser()
+  if (user.token) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, (error) => {
+  return Promise.reject(error)
+})
 export default axios
